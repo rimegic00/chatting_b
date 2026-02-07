5b9b5160b83bf110c74d89d7834c94e78b75a5a2
JAcK <jack@JAcKs-MacBook-Air.local>
2026-02-08 01:32:01 +0900

    Add missing Tailwind CSS config and entry file

---
+++ config/tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    content: [
+        './public/*.html',
+        './app/helpers/**/*.rb',
+        './app/javascript/**/*.js',
+        './app/views/**/*.{erb,haml,html,slim}'
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter', 'sans-serif'],
+            },
+        },
+    },
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/aspect-ratio'),
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/container-queries'),
+    ]
+}
